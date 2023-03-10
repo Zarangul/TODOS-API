92c3b9fcc13dbba417cea94da09e77999ff55513
Zarangul <zarangulbabazade@gmail.com>
2023-03-10 20:12:57 +0400

    bugfix/app.js/all-functions/style.scss/edit-btn-bgcolor

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,72 +10,76 @@ addButton.addEventListener("mouseleave", () => {
   addButton.style.backgroundColor = "darkblue";
 });
 
-addButton.addEventListener("click", () => {
-  let inValue = input.value.trim();
+addButton.addEventListener("click", postToDo);
+getData();
+function getData() {
+  fetch("https://crudapp-pwlck5pebq-el.a.run.app/api/todos")
+    .then((res) => res.json())
+    .then((data) =>
+      data.todos.forEach((e) => {
+        ul.innerHTML += `
+    <li id="in-li${e._id}" class="in-li" style="${
+          e.completed && "background-color: #e9f0f1"
+        }">
+        <span id="in-span-1${e._id}" style="${
+          e.completed && "text-decoration: line-through"
+        }">${e.title}</span>
+        <div class='edit-delete-icon'>
+          <span class='edit' onClick='updateToDo(${JSON.stringify(e)})'>
+            <i class="fa-regular fa-pen-to-square"></i>
+          </span>
+          <span class="in-span-2" onClick=deleteToDo('${e._id}')>
+            <i class="fa-sharp fa-solid fa-trash-can"></i>
+          </span>
+        </div> 
+    </li>
+    `;
+      })
+    );
+}
 
-  if (inValue !== "") {
-    fetch(API_URL, {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({ title: inValue }),
-    })
-      .then(() => {
+function postToDo() {
+  fetch("https://crudapp-pwlck5pebq-el.a.run.app/api/todos", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      title: input.value,
+      completed: false,
+    }),
+  })
+    .then((res) => {
+      if (res.status === 201) {
         ul.innerHTML = "";
         input.value = "";
-        write();
-      })
-      .catch((error) => console.error(error));
-    
-  } else {
-    alert("Please enter valid value");
-  }
-});
-
-function write() {
-  fetch(API_URL)
-    .then((resp) => resp.json())
-    .then((data) => {
-      console.log(data.todos[0].title);
-
-      data.todos.forEach((e) => {
-        ul.innerHTML += `<li id="in-li${e._id}" class="in-li">
-        <span id="in-span-1${e._id}">${e.title}</span>
-        <div>
-        <span class='edit' onClick=editLi(${`'${e._id}'`})><i class="fa-regular fa-pen-to-square"></i></span>
-        <span class="in-span-2" onClick=deletedLi(${`'${e._id}'`})><i class="fa-sharp fa-solid fa-trash-can"></i></span></div>
-        
-    </li>`;
-      });
-    });
-}
-
-function deletedLi(id) {
-  fetch(`${API_URL}/${id}`, {
-    method: "DELETE",
-  }).then(() => {
-    // Remove the deleted item from the DOM
-    document.getElementById(`in-li${id}`).remove();
-  })
-  .catch((error) => console.log(error));
-}
-
-function editLi(id){
-  let span = document.getElementById(`in-span-1${id}`);
-  span.style.textDecoration = 'line-through'
-  fetch(`${API_URL}/${id}`, {
-    method: "PUT",
-    headers: {
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify({ completed: false }),
-  })
-    .then(() => {
-      ul.innerHTML = "";
-      input.value = "";
-      write();
+        getData();
+      }
     })
-    .catch((error) => console.error(error));
+    .catch((err) => console.log(err));
 }
-write();
+
+function deleteToDo(id) {
+  fetch(`https://crudapp-pwlck5pebq-el.a.run.app/api/todos/${id}`, {
+    method: "DELETE",
+  }).then((res) => {
+    if (res.status === 200) {
+      ul.innerHTML = "";
+      getData();
+    }
+  });
+}
+
+function updateToDo(value) {
+  fetch(`https://crudapp-pwlck5pebq-el.a.run.app/api/todos/${value._id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      ...value,
+      completed: value.completed == false ? true : false,
+    }),
+  }).then((e) => {
+    if (e.status === 200) {
+      ul.innerHTML = "";
+      getData();
+    }
+  });
+}
